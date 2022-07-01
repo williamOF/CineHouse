7251ed38152c05ec0176f759056367ab4704103c
Maike William <maike.study@gmail.com>
2022-07-01 11:40:53 -0300

    porFilmeEmCartaz

--- functions/porFilmeEmCartaz.js
+++ functions/porFilmeEmCartaz.js
@@ -1,1 +1,17 @@
-// aqui devo criara um novo objeto 
+const catalogo = require('../database/catalogo.json')
+const findMovie = require('./buscarFilme')
+const salvar = require('./salvarFilme')
+
+module.exports= function PorEmCartaz(filme){
+
+    let filmeValue = findMovie(filme)
+    filmeValue.emCartaz = true
+    salvar(catalogo)
+    console.table(catalogo)
+
+}
+
+
+
+
+
