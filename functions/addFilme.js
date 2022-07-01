7a23b17da241322167b23db321cf5622744516ea
Maike William <maike.study@gmail.com>
2022-07-01 11:43:01 -0300

    addFilme.js

--- functions/addFilme.js
+++ functions/addFilme.js
@@ -1,41 +1,10 @@
-const fs = require('fs')
-const salvar = require('./salvarFilme')
-const catalogo = require('../database/catalogo.json')
-const buscarFilme = require('./buscarFilme')
-
-let propriedade = process.argv[2]
-let valor = process.argv[3]
 
 
-let verificarCodigo=(dado)=>{
-    if(dado){
-        return buscarFilme(dado)
-    }
-}
-function adcionarFilme(catalogo){
+// obter dados do usuário até q seja satisfeita todas as propriedades
 
-   let validação = verificarCodigo(valor)
 
-    if(validação){
-        console.error('Este codigo já foi cadastrado')
-    }else{
-        switch(propriedade){
-            case 'codigo':
-                catalogo.codigo = valor
-            break;
-            case 'anoDeLancamento':
-              catalogo.anoDeLancamento =valor
-            break;
-            case 'titulo':
-                catalogo.titulo = valor
-            break;
-           case 'duracao':
-                catalogo.duracao = valor
-            break;
-            case 'emCartaz':
-                catalogo.emCartaz = valor
-            break;
-        }
-    }
-}
+// criar um novo objeto com o formato do catalogo 
 
+
+
+// salvar este novo objeto no catalogo 
